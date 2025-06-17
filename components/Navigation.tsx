68bdfb8fd6eb6a5b24938f71346c39d95ce00533
adamsmithdev <adam.barrett.smith@gmail.com>
2025-06-16 21:04:43 -0400

    Updating headshot photo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,7 +11,7 @@ export default function Navigation() {
                 className="relative w-full h-0 pb"
             >
                 <Image
-                    src="/assets/profile.png"
+                    src="/assets/profile.jpeg"
                     alt="Profile Picture"
                     fill
                     sizes="100%"
